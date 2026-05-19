0048e5b3a2f78e7d4c85949cd2045ac0daaddf4d
tserikawa <tkt.srkw@gmail.com>
2026-05-19 22:17:49 +0900

    change:グリッドを表示するように変更

--- src/core/Renderer.cpp
+++ src/core/Renderer.cpp
@@ -10,8 +10,21 @@ void RenderAxis()
     DrawLineEx({20.0f, 35.0f}, {20.0f, 115.0f}, 2.0f, GREEN);
 }
 
+void RenderGrid()
+{
+    // https://github.com/raylib-extras/examples-c/blob/main/mouse_zoom/mouse_zoom.c
+    // draw the 3d grid, rotated 90 degrees and centered around 0,0 just so we have something in the XY plane
+    // DrawGrid関数だと色の変更はできない。
+    rlPushMatrix();
+    rlTranslatef(0, 25 * 50, 0);
+    rlRotatef(90, 1, 0, 0);
+    DrawGrid(100, 50);
+    rlPopMatrix();
+}
+
 void Render(const Point2f &mouse, const AppState &appState)
 {
+    RenderGrid();
     RenderAxis();
 
     Font font = GetFontDefault();
