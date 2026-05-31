b5a7ccac3b1e9fea8c942579c0f52f8f3d6017a5
tserikawa <tkt.srkw@gmail.com>
2026-05-31 19:32:00 +0900

    ControlPanelに実行中のコマンドを表示するように変更

--- src/core/CommandState.hpp
+++ src/core/CommandState.hpp
@@ -15,3 +15,32 @@ enum class CommandState
     RenumberPointByYAscending,
     RenumberPointByYDescending,
 };
+
+inline std::string GetCommandDisplayName(CommandState command) noexcept
+{
+    switch (command)
+    {
+    case CommandState::Neautral:
+        return "Neautral";
+    case CommandState::Quit:
+        return "Quit";
+    case CommandState::AddPoint:
+        return "AddPoint";
+    case CommandState::SelectPoint:
+        return "SelectPoint";
+    case CommandState::UnSelectAllPoints:
+        return "UnSelectAllPoints";
+    case CommandState::DeletePoint:
+        return "DeletePoint";
+    case CommandState::RenumberPointByXAscending:
+        return "RenumberPointByXAscending";
+    case CommandState::RenumberPointByXDescending:
+        return "RenumberPointByXDescending";
+    case CommandState::RenumberPointByYAscending:
+        return "RenumberPointByYAscending";
+    case CommandState::RenumberPointByYDescending:
+        return "RenumberPointByYDescending";
+    default:
+        return "UnKnown";
+    }
+}
--- src/ui/ControlPanel.cpp
+++ src/ui/ControlPanel.cpp
@@ -7,7 +7,9 @@ void ControlPanel::Draw(AppState &appState) const
 {
     ImGui::SetNextWindowSize({300, 400}, ImGuiCond_Always);
     ImGui::Begin(m_name.c_str(), nullptr, ImGuiWindowFlags_NoResize);
-    ImGui::SeparatorText("Current Status");
+    ImGui::SeparatorText("Current Command");
+    ImGui::Text(GetCommandDisplayName(appState.GetCommand()).c_str());
+    ImGui::SeparatorText("Message");
     ImGui::Text(appState.GetStatusMessage().c_str());
     ImGui::SeparatorText("Objects");
     ImGui::Text("Node Count: %d", appState.GetPointCount());
